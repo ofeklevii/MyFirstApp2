562427f79e016ef1eb26f5c789df1d22d8491fcf
ofeklevii <ofeklevi@sisma.org.il>
2023-12-20 02:22:58 +0200

    final

---
+++ app/src/main/java/com/example/myfirstapp2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.textview);
+        tv1=findViewById(R.id.textview1);
+        tv.setText("sec");
+        tv1.setText("first");
+        tv1.setTextColor(Color.CYAN);
+        tv.setTextColor(Color.BLUE);
+        tv.setTextSize(50);
+        tv1.setTextSize(50);
+        tv1.setWidth(200);
+        tv.setWidth(350);
+    }
+}
