3707153e83038b33aa20f75760a6fb35036e72eb
ofeklevii <ofeklevi@sisma.org.il>
2023-12-31 18:12:39 +0200

    final

--- app/src/main/java/com/example/myfirstapp2/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,12 +16,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=findViewById(R.id.textview);
-        tv1=findViewById(R.id.textview1);
-        tv.setText("sec");
-        tv1.setText("first");
-        tv1.setTextColor(Color.CYAN);
-        tv.setTextColor(Color.BLUE);
-        tv.setTextSize(50);
-        tv1.setTextSize(50);
+        tv1=findViewById(R.id.textview2);
+        tv.setText("1");
+        tv1.setText("2");
+        tv1.setTextColor(Color.MAGENTA);
+        tv.setTextColor(Color.GRAY);
+        tv.setTextSize(40);
+        tv1.setTextSize(40);
+    }
+
+    public void go(View view) {
+        tv.setText("1");
+        tv1.setText("2");
+    }
+
+    public void go1(View view) {
+        tv.setText("2");
+        tv1.setText("1");
+    }
+
+    public void go2(View view) {
+        tv.setTextColor(Color.MAGENTA);
+        tv1.setTextColor(Color.GRAY);
+    }
+
+    public void go3(View view) {
+        tv1.setTextColor(Color.RED);
+        tv.setTextColor(Color.YELLOW);
     }
 }
